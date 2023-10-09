6fe268a7139f2b54ba47c7774513f95c3f24635f
c9summit <st21204@ormiston.school.nz>
2023-10-09 15:55:31 +1300

    Backen of login and signup

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+session_start();
 
+    include("connection.php");
+    include("functions.php");
 
-
+    $user_data = check_login($con);
 
 ?>
 
@@ -13,6 +16,9 @@
 <body>
     <a href="logout.php">Logout</a>
 	<h1>This is the DASHBOARD</h1>
+
+    <br>
+	Hello, <?php echo $user_data['user_name']; ?>
 
 </body>
 </html>
