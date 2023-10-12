2dfedd364ce0b40b30ad71a2252d54f31399bc9f
c9summit <st21204@ormiston.school.nz>
2023-10-13 00:05:44 +1300

    Dashboard completed and code made more responsive

--- index.php
+++ index.php
@@ -12,13 +12,164 @@ session_start();
 <html>
 <head>
     <title>DASHBOARD</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
-    <a href="logout.php">Logout</a>
-	<h1>This is the DASHBOARD</h1>
 
-    <br>
-	Hello, <?php echo $user_data['user_name']; ?>
+    <style type='text/css'>
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap");
+*{
+  margin: 0;
+  padding: 0;
+  outline: none;
+  border: none;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+@font-face {
+  font-family: 'planet_kosmosregular';
+  src: url('plank___-webfont.woff2') format('woff2'),
+       url('plank___-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+
+}
+body{
+  background: rgb(233, 233, 233);
+}
+.container{
+  display: flex;
+  width: 1200px;
+  margin: auto;
+}
+nav{
+  position: absolute;
+  top: 0;
+  left: -20px;
+  bottom: 0;
+  width: 280px;
+  height: 100vh;
+  background: #fff;
+}
+.navbar{
+  width: 80%;
+  margin: 0 auto;
+}
+
+.logo{
+  margin: 2rem 0 1rem 0;
+  padding-bottom: 3rem;
+  display: flex;
+  align-items: center;
+}
+.logo img{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+.logo h1{
+  font-size: 40px;
+  margin: 10px 0 30px;
+  line-height: 80px;
+  color: darkred;
+  font-family: 'planet_kosmosregular', Arial, sans-serif;
+  margin-left: 1rem;
+}
+
+ul{
+  margin: 0 auto;
+}
+li{
+  padding-bottom: 2rem;
+}
+li a{
+  font-size: 16px;
+  color: rgb(85, 83, 83);
+}
+
+nav i{
+  width: 50px;
+  font-size: 18px;
+  text-align: center;
+}
+
+.logout{
+  position: absolute;
+  bottom: 20px;
+}
+
+.back-video{
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+}
+
+.hero{
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(rgba(12, 3, 51, 0.3),rgba(12, 3, 51, 0.3));
+  position: relative;
+  padding: 0 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+    </style>
+
+    <div class="hero">
+
+    <video autoplay loop muted plays-inline class="back-video">
+      <source src="media/balls.mp4" type="video/mp4">
+    </video>
+
+    <section id="dash">
+      <div class="display">
+        <div class='greeting'>Hello <?php echo $user_data['user_name']; ?></div>
+      </div>
+    </section>
+
+
+  </div>
+
+  <div class="container">
+    <nav>
+      <div class="navbar">
+        <div class="logo">
+          <h1>drive</h1>
+        </div>
+        <ul>
+          <li><a href="index.php">
+            <i class="fas fa-user"></i>
+            <span class="nav-item">Dashboard</span>
+          </a>
+          </li>
+          <li><a href="beginner.html">
+            <i class="fas fa-basketball-ball"></i>
+            <span class="nav-item">Beginner</span>
+          </a>
+          </li>
+          <li><a href="download.html">
+            <i class="fas fa-download"></i>
+            <span class="nav-item"> Download</span>
+          </a>
+        </li>
+          <li><a href="logout.php" class="logout">
+            <i class="fas fa-sign-out-alt"></i>
+            <span class="nav-item">Logout</span>
+          </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  </div>
+
 
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -32,117 +32,141 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Signup</title>
+	<title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 
 	<style type="text/css">
-    body {
-        background-image: url("media/bglg3.jpeg");
-        background-color: #cccccc;
+body {
+	background-color: #cccccc;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+}
 
-    }
+h2 {
+	font-size: 50px;
+	margin: 10px 0 30px;
+	line-height: 0;
+	color: black;
+	font-family: Sans-serif;
+	text-align: center;
+}
 
-    h1 {
-        font-size:50px;
-        margin: 10px 0 30px;
-        line-height: 0px;
-        color: white;
-        font-family: Sans-serif
-    }
+.login-box {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	max-width: 440px;
+	padding: 30px;
+	box-shadow: 0px 5px 10px 1px rgba(0, 0, 0, 0.05);
+	background-color: #8c9197;
+	border-radius: 20px;
+	text-align: center;
+    position:relative;
+}
 
-    .login-box{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 440px;
-    height: 480px;
-    padding: 30px;
-    }
-    .login-header{
-    text-align: center;
-    margin: 20px 0 40px 0;
-    }
-    .login-header header{
-    color: #333;
-    font-size: 30px;
-    font-weight: 600;
-    }
-    .input-box .input-field{
-    width: 100%;
-    height: 60px;
-    font-size: 17px;
-    padding: 0 25px;
-    margin-bottom: 15px;
-    border-radius: 30px;
-    border: none;
-    box-shadow: 0px 5px 10px 1px rgba(0,0,0, 0.05);
-    outline: none;
-    transition: .3s;
-    }
-    ::placeholder{
-    font-weight: 500;
-    color: #222;
-    }
-    .input-field:focus{
-    width: 105%;
-    }
-    section{
-    display: flex;
-    align-items: center;
-    font-size: 14px;
-    color: #555;
-    }
-    a{
-    text-decoration: none;
-    }
-    a:hover{
-    text-decoration: underline;
-    }
-    section a{
-    color: #555;
-    }
-    .input-submit{
-    position: relative;
-    }
-    .submit-btn{
-    width: 100%;
-    height: 60px;
-    background: #222;
-    border: none;
-    border-radius: 30px;
-    cursor: pointer;
-    transition: .3s;
-    }
-    .input-submit label{
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    color: #fff;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    cursor: pointer;
-    }
-    .submit-btn:hover{
-    background: #000;
-    transform: scale(1.05,1);
-    }
-    .sign-up-link{
-    text-align: center;
-    font-size: 15px;
-    margin-top: 20px;
-    }
-    .sign-up-link a{
-    color: #000;
-    }
+.login-header {
+	margin: 20px 0 40px 0;
+}
+
+.login-header header {
+	color: gray;
+	font-size: 30px;
+	font-weight: 600;
+}
+
+.input-box .input-field {
+	width: 80%;
+	height: 60px;
+	font-size: 17px;
+	padding: 0 25px;
+	margin-bottom: 15px;
+	border-radius: 30px;
+	border: none;
+	outline: none;
+	transition: .3s;
+}
+
+.input-field::placeholder {
+	font-weight: 500;
+	color: #222;
+}
+
+.input-field:focus {
+	width: 90%;
+}
+
+section {
+	display: flex;
+	align-items: center;
+	font-size: 14px;
+	color: gray;
+}
+
+a {
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
+
+section a {
+	color: gray;
+}
+
+.input-submit {
+	position: relative;
+}
+
+.submit-btn {
+	width: 100%;
+	height: 60px;
+	background: #222;
+	border: none;
+	border-radius: 30px;
+	cursor: pointer;
+	transition: .3s;
+}
+
+.input-submit label {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	color: #fff;
+	transform: translate(-50%, -50%);
+	cursor: pointer;
+}
+
+.submit-btn:hover {
+	background: #000;
+	transform: scale(1.05, 1);
+}
+
+.sign-up-link {
+	text-align: center;
+	font-size: 15px;
+	margin-top: 20px;
+}
+
+.sign-up-link a {
+	color: #000;
+}
+
 
 	</style>
+
     <div id="box">
         <form method="post">
             <div class="login-box">
                 <div class="login-header">
-                    <h1>Signup</h1>
+                    <h2>Signup</h2>
                 </div>
                 <div class="input-box">
                     <input type="text" class="input-field" placeholder="Username" autocomplete="off" name="user_name" required>
@@ -157,10 +181,9 @@ session_start();
                     <label for="button">Signup</label>
                 </div>
                 <div class="sign-up-link">
-                    <p>Have an account? <a href="login.php">Login</a></p>
+                    <p>Already have an account? <a href="login.php">Login</a></p>
                 </div>
             </div>
     </div>
 </body>
 </html>
-
